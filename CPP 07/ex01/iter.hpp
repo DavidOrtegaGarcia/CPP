679153468cd130eac5cd70c880cdb2cf2fd8cd8a
DavidOrtegaGarcia <daorga77@gmail.com>
2025-10-22 22:17:25 +0200

    Const version done

--- CPP 07/ex01/iter.hpp
+++ CPP 07/ex01/iter.hpp
@@ -3,16 +3,32 @@
 #include <iostream>
 
 template<typename T>
-void print_array_value(T data)
+void print_array_value(T &data)
 {
 	std::cout << data << " ";
 }
 
 template<typename T>
-void iter(T *array, int length, void (*function)(T))
+void print_array_value(const T &data)
+{
+	std::cout << data << " ";
+}
+
+template<typename T>
+void iter(T *array, const int length, void (*function)(T&))
 {
 	for(int i = 0; i < length; i++)
 	{
 		function(array[i]);
 	}
 }
+
+template<typename T>
+void iter(const T *array, const int length, void (*function)(const T&))
+{
+	for(int i = 0; i < length; i++)
+	{
+		function(array[i]);
+	}
+}
+
--- CPP 07/ex01/main.cpp
+++ CPP 07/ex01/main.cpp
@@ -6,7 +6,10 @@ int main(void)
 	int i[5] = {1, 2, 3, 4, 5};
 	float f[5] ={0.11, 0.12, 0.13, 0.14, 0.15};
 	double d[5] ={0.16, 0.17, 0.18, 0.19, 0.2};
+	float cf[5] ={0.11, 0.12, 0.13, 0.14, 0.15};
+	double cd[5] ={0.16, 0.17, 0.18, 0.19, 0.2};
 
+	std::cout << "Non const arrays" << std::endl;
 	iter(c, 5, print_array_value);
 	std::cout << std::endl;
 	iter(i, 5, print_array_value);
@@ -14,5 +17,10 @@ int main(void)
 	iter(f, 5, print_array_value);
 	std::cout << std::endl;
 	iter(d, 5, print_array_value);
+	std::cout << std::endl;
+	std::cout << "Const arrays" << std::endl;
+	iter(cf, 5, print_array_value);
+	std::cout << std::endl;
+	iter(cd, 5, print_array_value);
 	std::cout << std::endl;
 }
